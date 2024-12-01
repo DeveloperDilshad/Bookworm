85012942ff5252b4c43bc4f336baccd22fe7f97d
Developer Dilshad <developerdilshadp@gmail.com>
2024-12-01 17:15:54 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Dilshad P on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
